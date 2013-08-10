67137fe0f80af0f45666655208556b2ca5d13b0d
zwbn57175596 <eblis.e.a.zhao@gmail.com>
2013-08-10 10:51:21 +0800

    网站抓取框架Crawler4j例子

---
+++ DMM_crawler4j/src/com/zhaowei/dmm/DDMCrawlerMain.java
@@ -0,0 +1,46 @@
+package com.zhaowei.dmm;
+
+import com.zhaowei.dmm.crawler4j.impl.DDMCrawlerImpl;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class DDMCrawlerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		String crawlStorageFolder = "/home/zhaowei/Documents/ddm";
+		int numberOfCrawlers = 7;
+
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder(crawlStorageFolder);
+
+		/*
+		 * Instantiate the controller for this crawl.
+		 */
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+		/*
+		 * For each crawl, you need to add some seed urls. These are the first
+		 * URLs that are fetched and then the crawler starts following links
+		 * which are found in these pages
+		 */
+		controller.addSeed("http://www.ddm.org.tw/");
+
+		/*
+		 * Start the crawl. This is a blocking operation, meaning that your code
+		 * will reach the line after this only when crawling is finished.
+		 */
+		controller.start(DDMCrawlerImpl.class, numberOfCrawlers);
+
+	}
+
+}
