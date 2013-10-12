b8f3ab178e3051c58afbc67edbda5ac8ee1ac13a
zwbn57175596 <eblis.e.a.zhao@gmail.com>
2013-10-12 16:52:24 +0800

    python study
    python study

---
+++ Python/helloworld/expression.py
@@ -0,0 +1,7 @@
+# Filename : expression.py
+
+length = 5
+breadth = 2
+area = length * breadth
+print 'Area is', area
+print 'Perimter is ', 2 * (length * breadth)
---
+++ Python/helloworld/var.py
@@ -0,0 +1,9 @@
+# Filename : var.py
+i = 5
+print i
+i = i + 1
+print i
+
+s = '''This is a multi-link string.
+This is the second line.'''
+print s
