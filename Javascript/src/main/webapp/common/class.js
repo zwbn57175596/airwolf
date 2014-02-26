ee77dfc5da31e0cec5b6fd9dd8e2e06d21c432d7
zwbn57175596 <eblis.e.a.zhao@gmail.com>
2014-02-26 11:48:43 +0800

    study for node js

---
+++ Javascript/src/main/webapp/common/class.js
@@ -0,0 +1,33 @@
+var Class = function () {
+	var klass = function () {
+		this.init.apply(this, arguments);
+	};
+	
+	klass.prototype.init = function () {};
+	
+	// 定义prototype别名
+	klass.fn = klass.prototype;
+	
+	// 定义类的别名
+	klass.fn.parent = klass;
+	
+	// 给类添加属性
+	klass.extend = function (obj) {
+		var extended = obj.extended;
+		for (var i in obj) {
+			klass[i] = obj[i];
+		}
+		if (extended) extended(klass);
+	};
+	
+	// 给实例添加属性
+	klass.include = function (obj) {
+		var included = obj.included;
+		for (var i in obj) {
+			klass.fn[i] = obj[i];
+		}
+		if (included) included(klass);
+	};
+	
+	return klass;
+};
