b8a7897c6fc0d3fcafb7d3323a269365e08d795e
zhaoweih <zhaoweih@chanjet.com>
2015-12-07 15:00:07 +0800

    practise for java struct 2nd edition
          chap05
              linkList2  format

--- exam-practise/src/chap05/linkList2.java
+++ exam-practise/src/chap05/linkList2.java
@@ -4,115 +4,116 @@ package chap05;
 // demonstrates linked list
 // to run this program: C>java LinkList2App
 ////////////////////////////////////////////////////////////////
-class Link2
-   {
-   public int iData;              // data item (key)
-   public double dData;           // data item
-   public Link2 next;              // next link in list
-// -------------------------------------------------------------
-   public Link2(int id, double dd) // constructor
-      {
-      iData = id;
-      dData = dd;
-      }
-// -------------------------------------------------------------
-   public void displayLink()      // display ourself
-      {
-      System.out.print("{" + iData + ", " + dData + "} ");
-      }
-   }  // end class Link2
+class Link2 {
+  public int iData;              // data item (key)
+  public double dData;           // data item
+  public Link2 next;              // next link in list
+
+  // -------------------------------------------------------------
+  public Link2(int id, double dd) // constructor
+  {
+    iData = id;
+    dData = dd;
+  }
+
+  // -------------------------------------------------------------
+  public void displayLink()      // display ourself
+  {
+    System.out.print("{" + iData + ", " + dData + "} ");
+  }
+}  // end class Link2
+
 ////////////////////////////////////////////////////////////////
-class LinkList2
-   {
-   private Link2 first;            // ref to first link on list
+class LinkList2 {
+  private Link2 first;            // ref to first link on list
 
-// -------------------------------------------------------------
-   public LinkList2()              // constructor
-      {
-      first = null;               // no links on list yet
+  // -------------------------------------------------------------
+  public LinkList2()              // constructor
+  {
+    first = null;               // no links on list yet
+  }
+
+  // -------------------------------------------------------------
+  public void insertFirst(int id, double dd) {                           // make new link
+    Link2 newLink = new Link2(id, dd);
+    newLink.next = first;       // it points to old first link
+    first = newLink;            // now first points to this
+  }
+
+  // -------------------------------------------------------------
+  public Link2 find(int key)      // find link with given key
+  {                           // (assumes non-empty list)
+    Link2 current = first;              // start at 'first'
+    while (current.iData != key)        // while no match,
+    {
+      if (current.next == null)        // if end of list,
+        return null;                 // didn't find it
+      else                            // not end of list,
+        current = current.next;      // go to next link
+    }
+    return current;                    // found it
+  }
+
+  // -------------------------------------------------------------
+  public Link2 delete(int key)    // delete link with given key
+  {                           // (assumes non-empty list)
+    Link2 current = first;              // search for link
+    Link2 previous = first;
+    while (current.iData != key) {
+      if (current.next == null)
+        return null;                 // didn't find it
+      else {
+        previous = current;          // go to next link
+        current = current.next;
       }
-// -------------------------------------------------------------
-   public void insertFirst(int id, double dd)
-      {                           // make new link
-      Link2 newLink = new Link2(id, dd);
-      newLink.next = first;       // it points to old first link
-      first = newLink;            // now first points to this
-      }
-// -------------------------------------------------------------
-   public Link2 find(int key)      // find link with given key
-      {                           // (assumes non-empty list)
-      Link2 current = first;              // start at 'first'
-      while(current.iData != key)        // while no match,
-         {
-         if(current.next == null)        // if end of list,
-            return null;                 // didn't find it
-         else                            // not end of list,
-            current = current.next;      // go to next link
-         }
-      return current;                    // found it
-      }
-// -------------------------------------------------------------
-   public Link2 delete(int key)    // delete link with given key
-      {                           // (assumes non-empty list)
-      Link2 current = first;              // search for link
-      Link2 previous = first;
-      while(current.iData != key)
-         {
-         if(current.next == null)
-            return null;                 // didn't find it
-         else
-            {
-            previous = current;          // go to next link
-            current = current.next;
-            }
-         }                               // found it
-      if(current == first)               // if first link,
-         first = first.next;             //    change first
-      else                               // otherwise,
-         previous.next = current.next;   //    bypass it
-      return current;
-      }
-// -------------------------------------------------------------
-   public void displayList()      // display the list
-      {
-      System.out.print("List (first-->last): ");
-      Link2 current = first;       // start at beginning of list
-      while(current != null)      // until end of list,
-         {
-         current.displayLink();   // print data
-         current = current.next;  // move to next link
-         }
-      System.out.println("");
-      }
-// -------------------------------------------------------------
-   }  // end class LinkList2
+    }                               // found it
+    if (current == first)               // if first link,
+      first = first.next;             //    change first
+    else                               // otherwise,
+      previous.next = current.next;   //    bypass it
+    return current;
+  }
+
+  // -------------------------------------------------------------
+  public void displayList()      // display the list
+  {
+    System.out.print("List (first-->last): ");
+    Link2 current = first;       // start at beginning of list
+    while (current != null)      // until end of list,
+    {
+      current.displayLink();   // print data
+      current = current.next;  // move to next link
+    }
+    System.out.println("");
+  }
+  // -------------------------------------------------------------
+}  // end class LinkList2
+
 ////////////////////////////////////////////////////////////////
-class LinkList2App
-   {
-   public static void main(String[] args)
-      {
-      LinkList2 theList = new LinkList2();  // make list
+class LinkList2App {
+  public static void main(String[] args) {
+    LinkList2 theList = new LinkList2();  // make list
 
-      theList.insertFirst(22, 2.99);      // insert 4 items
-      theList.insertFirst(44, 4.99);
-      theList.insertFirst(66, 6.99);
-      theList.insertFirst(88, 8.99);
+    theList.insertFirst(22, 2.99);      // insert 4 items
+    theList.insertFirst(44, 4.99);
+    theList.insertFirst(66, 6.99);
+    theList.insertFirst(88, 8.99);
 
-      theList.displayList();              // display list
+    theList.displayList();              // display list
 
-      Link2 f = theList.find(44);          // find item
-      if( f != null)
-         System.out.println("Found link with key " + f.iData);
-      else
-         System.out.println("Can't find link");
+    Link2 f = theList.find(44);          // find item
+    if (f != null)
+      System.out.println("Found link with key " + f.iData);
+    else
+      System.out.println("Can't find link");
 
-      Link2 d = theList.delete(66);        // delete item
-      if( d != null )
-         System.out.println("Deleted link with key " + d.iData);
-      else
-         System.out.println("Can't delete link");
+    Link2 d = theList.delete(66);        // delete item
+    if (d != null)
+      System.out.println("Deleted link with key " + d.iData);
+    else
+      System.out.println("Can't delete link");
 
-      theList.displayList();              // display list
-      }  // end main()
-   }  // end class LinkList2App
+    theList.displayList();              // display list
+  }  // end main()
+}  // end class LinkList2App
 ////////////////////////////////////////////////////////////////
