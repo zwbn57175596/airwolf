bc363b1dadebfff0a1525afa22c383ce8977b776
赵炜 <zhaoweih@chanjet.com>
2017-11-28 18:57:43 +0800

    spring cloud config save

--- spring-cloud-study/rest-client/src/main/java/edu/ndfz/study/spring/cloud/rest/client/controller/HelloController.java
+++ spring-cloud-study/rest-client/src/main/java/edu/ndfz/study/spring/cloud/rest/client/controller/HelloController.java
@@ -1,6 +1,7 @@
 package edu.ndfz.study.spring.cloud.rest.client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,13 @@ public class HelloController {
 
   @Autowired
   private RestTemplate restTemplate;
+
+  @Value("${foo}") String foo;
+
+  @RequestMapping(value = "/hi", method = RequestMethod.GET)
+  public String hi() {
+    return foo;
+  }
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String hello() {
