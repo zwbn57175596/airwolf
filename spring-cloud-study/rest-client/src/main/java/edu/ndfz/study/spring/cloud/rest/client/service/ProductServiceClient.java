1c27f52c0406bf8530d88d9b465600b7a7c0e04c
赵炜 <zhaoweih@chanjet.com>
2017-11-28 10:23:00 +0800

    搞完了，还加上了Fegin和熔断

--- spring-cloud-study/rest-client/src/main/java/edu/ndfz/study/spring/cloud/rest/client/service/ProductServiceClient.java
+++ spring-cloud-study/rest-client/src/main/java/edu/ndfz/study/spring/cloud/rest/client/service/ProductServiceClient.java
@@ -1,9 +1,25 @@
 package edu.ndfz.study.spring.cloud.rest.client.service;
 
 import edu.ndfz.study.spring.cloud.rest.core.IProductService;
+import edu.ndfz.study.spring.cloud.rest.core.Product;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient("REST-API")
+import java.util.List;
+
+@FeignClient(value = "REST-API", fallback = ComputeClientHystrix.class)
 public interface ProductServiceClient extends IProductService {
 
+}
+
+class ComputeClientHystrix implements IProductService {
+
+  @Override
+  public List<Product> list() {
+    return null;
+  }
+
+  @Override
+  public Product detail(String itemCode) {
+    return null;
+  }
 }
